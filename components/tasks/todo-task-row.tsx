a227c89dd2b5baa596db40b30286d6d8b2db972a
Velvet <velvet.pinkish@gmail.com>
2026-03-06 11:08:30 +0100

    Create props for components

---
+++ components/tasks/todo-task-row.tsx
@@ -0,0 +1,9 @@
+import { Task } from "@/types/task";
+
+type TodoTaskRowProps = {
+    task: Task
+}
+
+export default function TodoTaskRow({ task }: TodoTaskRowProps) {
+    
+}
--- components/ui/todo-list-grid.tsx
+++ components/ui/todo-list-grid.tsx
@@ -1,6 +1,10 @@
 import type { ReactNode } from "react";
 import { Task } from "@/types/task"
 
+type TodoListProps = {
+    tasks: Task[];
+}
+
 export default function TodoGrid(props) {
     return(
 
