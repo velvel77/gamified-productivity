e72735a5177f38874ac303dd8529661d761ecc44
Velvet <velvet.pinkish@gmail.com>
2026-03-10 15:09:58 +0100

    Create type for character

---
+++ components/ui/main-character-widget.tsx
@@ -0,0 +1,1 @@
+import type { Character } from "@/types/character";
---
+++ types/character.ts
@@ -0,0 +1,8 @@
+export type Character = {
+    id: string
+    name: string
+    level: number
+    exp: number
+    health: number
+    isNewlyRevived: boolean
+}
