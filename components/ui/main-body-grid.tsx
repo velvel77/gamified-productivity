2825c3002454a4ca1c2d3f5c22c93fac12899d7c
Velvet <velvet.pinkish@gmail.com>
2026-03-10 10:05:06 +0100

    Add conditional placeholder text in task list

--- components/ui/main-body-grid.tsx
+++ components/ui/main-body-grid.tsx
@@ -6,7 +6,7 @@ type MainBodyGridProps = {
 
 export default function MainBodyGrid({children}: MainBodyGridProps ) {
     return (
-        <div className=" border-red-500 border-4 flex flex-col items-center mt-8 space-y-5">
+        <div className="flex flex-col items-center mt-8 space-y-5">
             {children}
         </div>
     )
