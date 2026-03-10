67aeacb94b09b00475f8202da3021dde3d8c2809
Velvet <velvet.pinkish@gmail.com>
2026-03-10 10:00:45 +0100

    Make default list much broader and add min height

--- components/ui/todo-list-grid.tsx
+++ components/ui/todo-list-grid.tsx
@@ -8,8 +8,9 @@ type TodoTasksProps = {
 export default function TodoGrid({ tasks }: TodoTasksProps) {
     return(
         //TODO: implement auto-columns
-        <div className="grid m-w-full justify-items-center">
-            <ul className="max-w-7/8 min-w-6/8 flex flex-col mt-6 p-3 space-y-2 bg-white border rounded-xl">
+        <div className="grid w-full justify-items-center  border-red-500 border-4">
+            <ul className="max-w-7/8 min-w-6/8 min-h-30 flex flex-col mt-6 p-3 space-y-2 bg-white border rounded-xl">
+                
                 {tasks.map((task) => (
                     <TodoTaskRow key={task.id} task={task} />
                 ))}
