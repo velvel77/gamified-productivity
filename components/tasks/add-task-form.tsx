ab2abb3e3d06690b3b5fdc059dfa649362ebb70b
Velvet <velvet.pinkish@gmail.com>
2026-03-10 13:52:00 +0100

    Add accessibility for form

--- components/tasks/add-task-form.tsx
+++ components/tasks/add-task-form.tsx
@@ -4,9 +4,14 @@ import { addTask } from "@/actions/actions";
 export default function AddTaskForm() {
     return(
         <form action={addTask} className="space-x-2 h-4">
+            <label htmlFor="title" className="sr-only">
+                Add task
+            </label>
             <input 
+            id="title"
             type="text" 
             name="title" 
+            placeholder="New title"
             className="bg-white px-3 py-1 rounded" />
             <button type="submit" className="bg-background px-3 py-1 text-black rounded-lg">
               Add task
