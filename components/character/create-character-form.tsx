a180c0f27f67316ba109a6216d1cb0594113cb6d
Velvet <velvet.pinkish@gmail.com>
2026-03-12 10:32:02 +0100

    Implement create character

--- components/character/create-character-form.tsx
+++ components/character/create-character-form.tsx
@@ -2,21 +2,23 @@ import { createCharacter } from "@/actions/actions";
 
 export default function CreateCharacterForm() {
     return(
-        <form action={createCharacter} className="space-x-2 h-4">
-            <label htmlFor="name" className="sr-only">
-                Create character
-            </label>
-            <input
-            id="name"
-            type="text"
-            name="name"
-            placeholder="New character"
-            className="bg-white px-3 py-1 rounded" />
-        <button
-        className=""
-        type="submit">
-            Create Character
-        </button>
-        </form>
+        <div className="flex p-5 justify-center">
+            <form action={createCharacter } className="flex justify-center space-x-2">
+                <label htmlFor="name" className="sr-only">
+                    Create character
+                </label>
+                <input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="New character"
+                className="max-w-3/5 bg-white px-3 py-1 rounded" />
+                <button
+                className="bg-white"
+                type="submit">
+                    Create Character
+                </button>
+            </form>
+        </div>
     )
 }
--- components/ui/main-character-widget.tsx
+++ components/ui/main-character-widget.tsx
@@ -1,21 +1,43 @@
+import { deleteCharacter } from "@/actions/actions";
 import type { Character } from "@/types/character";
+import CreateCharacterForm from "../character/create-character-form";
 
 type CharacterProps = {
-    character: Character | null;
+  character: Character | null;
 };
 
 export default function MainCharacterWidget({ character }: CharacterProps) {
-    if (!character) {
-        return(
-            <div className="flex justify-center p-5 bg-background w-full">
-                <span className="text-black">Create your character!</span>
-            </div>
-            )
-    }
-    
-    return(
-        <div className="flex justify-center p-5 bg-background">
-            <span className="">Health:</span> {character.health}
+  if (!character) {
+    return (
+      <div className="flex flex-col justify-center items-center p-5 bg-background w-full">
+        <span className="text-black">Create your character!</span>
+        <CreateCharacterForm />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center justify-between p-5 bg-background">
+        <div>
+            <span className="text-white"></span>
         </div>
-    )
+        <section className="flex flex-col justify-center items-center">
+            <div className="flex gap-4">
+                <span className="text-white">Lv: {character.level}</span>
+                <span className="text-white">Exp: {character.exp}/50</span>
+            </div>
+            <span className="text-white">Health: {character.health}</span>
+        </section>
+        <div>
+            <form action={deleteCharacter}>
+                <input type="hidden" name="id" value={character.id} />
+                <button
+                    className="bg-white hover:bg-pink-500 rounded-xl cursor-pointer p-1.5"
+                    type="submit"
+                    aria-label="Delete character"
+                >X</button>
+            </form>
+        </div>
+    </div>
+  );
 }
